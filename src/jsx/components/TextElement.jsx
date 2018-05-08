72bf1dabb9c85600f444d2b1004bec27a82fe60d
Christian Laesser <mail@christianlaesser.com>
2018-05-08 10:33:05 +0200

    Textbar: Scrolling inbetween step
    Scrolling works but need bug fixes

--- src/jsx/components/TextElement.jsx
+++ src/jsx/components/TextElement.jsx
@@ -1,53 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const TextElement = props => {
-  const {
-    id,
-    text,
-    annotations,
-    isActive,
-    isHovered,
-    isSelected,
-    onHover,
-    onClick,
-  } = props;
+class TextElement extends Component {
+  render() {
+    const {
+      id,
+      text,
+      annotations,
+      isActive,
+      isHovered,
+      isSelected,
+      onHover,
+      onClick,
+    } = this.props;
 
-  let annotationsGradient = '';
-  let backgroundColor = isHovered || isSelected ? '#eee' : 'white';
-  annotations.forEach((d, i) => {
-    const color = d.active == undefined || d.active ? d.color : '#ddd';
-    annotationsGradient += `,${color} ${i * 2}px, ${backgroundColor} ${i * 2 +
-      1}px, ${backgroundColor} ${i * 2 + 2}px`;
-  });
+    let annotationsGradient = '';
+    let backgroundColor = isHovered || isSelected ? '#eee' : 'white';
+    annotations.forEach((d, i) => {
+      const color = d.active == undefined || d.active ? d.color : '#ddd';
+      annotationsGradient += `,${color} ${i * 2}px, ${backgroundColor} ${i * 2 +
+        1}px, ${backgroundColor} ${i * 2 + 2}px`;
+    });
 
-  return (
-    <span
-      className={`${!isActive ? 'is-inactive' : ''} ${isHovered
-        ? 'is-hovered'
-        : ''} ${isSelected ? 'is-selected' : ''}
-        : ''} ${annotations.length != 0 ? 'is-selectable' : ''}`}
-      style={
-        annotations.length ? (
-          {
-            lineHeight: `${22 + annotations.length * 2}px`,
-            background: `linear-gradient(0deg ${annotationsGradient})`,
-            backgroundPosition: '0 100%',
-            paddingBottom: annotations.length * 2,
-          }
-        ) : (
-          {}
-        )
-      }
-      onMouseOver={() => {
-        onHover(annotations.filter(d => d.active).map(d => d.id));
-      }}
-      onMouseOut={() => onHover()}
-      onClick={() => {
-        onClick(annotations.filter(d => d.active).map(d => d.id));
-      }}>
-      {text}
-    </span>
-  );
-};
+    return (
+      <span
+        className={`${!isActive ? ' is-inactive ' : ''}${isHovered
+          ? ' is-hovered '
+          : ''}${isSelected ? ' is-selected ' : ''}${annotations.length != 0
+          ? ' is-selectable '
+          : ''}`}
+        style={
+          annotations.length ? (
+            {
+              lineHeight: `${22 + annotations.length * 2}px`,
+              background: `linear-gradient(0deg ${annotationsGradient})`,
+              backgroundPosition: '0 100%',
+              paddingBottom: annotations.length * 2,
+            }
+          ) : (
+            {}
+          )
+        }
+        onMouseOver={() => {
+          onHover(annotations.filter(d => d.active).map(d => d.id));
+        }}
+        onMouseOut={() => onHover()}
+        onClick={() => {
+          onClick(annotations.filter(d => d.active).map(d => d.id));
+        }}>
+        {text}
+      </span>
+    );
+  }
+}
 
 export default TextElement;
--- src/jsx/containers/TextBar.jsx
+++ src/jsx/containers/TextBar.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { observer } from 'mobx-react';
 
 import dataAPI from 'data/dataAPI';
@@ -7,82 +8,146 @@ import config from 'config/config';
 
 import TextElement from '../components/TextElement';
 
-const TextBar = observer(props => {
-  // vars
-  const { activeTextElements } = dataAPI;
-  const { selectedAnnotationIds } = uiState;
-  const { TEXT_BAR_WIDTH } = config;
-
-  const selectedAnnotationIdsCount = selectedAnnotationIds.length;
-  const hasMoreSelectedAnnotations = selectedAnnotationIdsCount > 1;
-
-  // interactions
-  const handleTextHover = ids => uiState.setHoveredAnnotation(ids);
-  const handleTextSelect = ids => uiState.changeSelectedAnnotation(ids);
-  const handleSelectedAnnotationReset = ids =>
-    uiState.resetSelectedAnnotation();
-
-  // content
-  let textEls;
-  if (!hasMoreSelectedAnnotations) {
-    // normal text
-    textEls = activeTextElements.map(d => {
-      return (
-        <TextElement
-          key={d.id}
-          text={d.text}
-          annotations={d.annotations}
-          isActive={d.active}
-          isHovered={d.hovered}
-          isSelected={d.selected}
-          onHover={handleTextHover}
-          onClick={handleTextSelect}
-        />
-      );
-    });
-  } else {
-    // selected text +1
-    textEls = activeTextElements.map(group => {
-      return (
-        <div key={group.id} className="c-text-area__selected-text">
-          {group.items.map(d => {
-            return (
-              <TextElement
-                key={d.id}
-                text={d.text}
-                annotations={d.annotations}
-                isActive={d.active}
-                isHovered={d.hovered}
-                isSelected={false}
-                onHover={handleTextHover}
-                onClick={handleTextSelect}
-              />
-            );
-          })}
-        </div>
-      );
-    });
+@observer
+class TextBar extends Component {
+  constructor(props) {
+    super(props);
+    this.lastSelectedText = undefined;
+    this.selectedText = undefined;
+    this.textWrapper = undefined;
   }
 
-  // render
-  return (
-    <aside className="l-content-container" style={{ width: TEXT_BAR_WIDTH }}>
-      <header className="c-header--small">
-        <h2>Text View</h2>
-        {selectedAnnotationIdsCount != 0 && (
-          <span
-            className="c-header__right_element o-link"
-            onClick={handleSelectedAnnotationReset}>
-            (Reset)
-          </span>
-        )}
-      </header>
-      <div className="l-content-spacing">
-        <div className="c-text-nav" />
-        <div className="c-text-area">{textEls}</div>
-      </div>
-    </aside>
-  );
-});
+  render() {
+    // vars
+    const { activeTextElements } = dataAPI;
+    const { selectedAnnotationIds } = uiState;
+    const { TEXT_BAR_WIDTH } = config;
+
+    const selectedAnnotationIdsCount = selectedAnnotationIds.length;
+    const hasMoreSelectedAnnotations = selectedAnnotationIdsCount > 1;
+
+    // interactions
+    const handleTextHover = ids => uiState.setHoveredAnnotation(ids);
+    const handleTextSelect = ids => uiState.changeSelectedAnnotation(ids);
+    const handleSelectedAnnotationReset = ids =>
+      uiState.resetSelectedAnnotation();
+
+    // content
+    let textEls;
+    if (!hasMoreSelectedAnnotations) {
+      // normal text
+      textEls = activeTextElements.map(d => {
+        return (
+          <TextElement
+            key={d.id}
+            text={d.text}
+            annotations={d.annotations}
+            isActive={d.active}
+            isHovered={d.hovered}
+            isSelected={d.selected}
+            onHover={handleTextHover}
+            onClick={handleTextSelect}
+            ref={ref => {
+              if (
+                d.selected &&
+                this.selectedText == undefined &&
+                this.lastSelectedText != d.id
+              ) {
+                this.selectedText = ref;
+                this.lastSelectedText = d.id;
+              }
+            }}
+          />
+        );
+      });
+    } else {
+      // selected text +1
+      textEls = activeTextElements.map(group => {
+        return (
+          <div key={group.id} className="c-text-area__selected-text">
+            {group.items.map(d => {
+              return (
+                <TextElement
+                  key={d.id}
+                  text={d.text}
+                  annotations={d.annotations}
+                  isActive={d.active}
+                  isHovered={d.hovered}
+                  isSelected={false}
+                  onHover={handleTextHover}
+                  onClick={handleTextSelect}
+                  ref={ref => {
+                    if (d.selected && this.selectedText == undefined) {
+                      this.selectedText = ref;
+                    }
+                  }}
+                />
+              );
+            })}
+          </div>
+        );
+      });
+    }
+
+    // render
+    return (
+      <aside className="l-content-container" style={{ width: TEXT_BAR_WIDTH }}>
+        <header className="c-header--small">
+          <h2>Text View</h2>
+          {selectedAnnotationIdsCount != 0 && (
+            <span
+              className="c-header__right_element o-link"
+              onClick={handleSelectedAnnotationReset}>
+              (Reset)
+            </span>
+          )}
+        </header>
+        <div className="l-content-spacing">
+          <div className="c-text-nav" />
+          <div
+            className="c-text-area"
+            ref={x => {
+              this.textWrapper = x;
+            }}>
+            {textEls}
+          </div>
+        </div>
+      </aside>
+    );
+  }
+
+  componentDidUpdate() {
+    const panel = this.textWrapper;
+    let node = undefined;
+
+    if (this.selectedText) {
+      // hovered text
+      node = ReactDOM.findDOMNode(this.selectedText);
+      this.selectedText = undefined;
+    }
+
+    if (node) {
+      if (
+        node.offsetTop > panel.scrollTop + panel.offsetHeight ||
+        node.offsetTop < panel.scrollTop
+      ) {
+        //panel.scrollTop = node.offsetTop - panel.offsetTop;
+        this.scrollToEl(panel, node.offsetTop - panel.offsetTop, 300);
+      }
+    }
+  }
+
+  scrollToEl(element, to, duration) {
+    if (duration <= 0) return;
+    const difference = to - element.scrollTop;
+    const perTick = difference / duration * 10;
+
+    setTimeout(() => {
+      element.scrollTop = element.scrollTop + perTick;
+      if (element.scrollTop === to) return;
+      this.scrollToEl(element, to, duration - 10);
+    }, 10);
+  }
+}
 
 export default TextBar;
