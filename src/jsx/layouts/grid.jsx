dcf4546adfd317732a20cbcb4f9b6a7fb0e43bf3
Christian Laesser <mail@christianlaesser.com>
2018-05-15 13:17:29 +0200

    Layout: Grid fixed

--- src/jsx/layouts/grid.jsx
+++ src/jsx/layouts/grid.jsx
@@ -5,17 +5,42 @@ export default {
   title: 'Grid',
   inputs: [],
   create: function grid(glyphs, width, height, options) {
-    const space = 20;
+    let largestRadiusPerRow = 0;
+    const space = 5;
     let xSpace = 0;
     let ySpace = 0;
+    let yPos = 0;
+
+    const yPositions = {};
     const output = [];
-    orderBy(glyphs, (d, i) => d.tagVersion).map(glyph => {
+
+    // calc x position
+    orderBy(glyphs, 'tagVersion').map(glyph => {
       if (xSpace + glyph.radius * 2 > width) {
-        ySpace += glyph.radius * 2 + space;
+        ySpace += largestRadiusPerRow * 2 + space;
+        yPositions[yPos] = ySpace;
+        yPos += 1;
+        largestRadiusPerRow = 0;
         xSpace = 0;
       }
-      output.push({ ...glyph, x: xSpace, y: ySpace });
+      output.push({
+        ...glyph,
+        x: xSpace + glyph.radius,
+        y: yPos,
+      });
       xSpace += glyph.radius * 2 + space;
+      largestRadiusPerRow = Math.max(largestRadiusPerRow, glyph.radius);
+    });
+    if (xSpace > 0) {
+      ySpace += largestRadiusPerRow * 2 + space;
+      yPositions[yPos] = ySpace;
+    }
+
+    // optimize y position
+    output.forEach(d => {
+      if (d.y != 0) {
+        d.y = yPositions[d.y - 1] + (yPositions[d.y] - yPositions[d.y - 1]) / 2;
+      }
     });
     return output;
   },
