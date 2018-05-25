97222328f5c786c0c2924511a56159e9745a2adc
Christian Laesser <mail@christianlaesser.com>
2018-05-25 14:29:32 +0200

    CanvasForm: fixed

--- src/jsx/components/CanvasForm.jsx
+++ src/jsx/components/CanvasForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { observer } from 'mobx-react';
 
 @observer
-class CanvasForm extends React.PureComponent {
+class CanvasForm extends React.Component {
   constructor(props) {
     super(props);
     const { title, layout } = props;
