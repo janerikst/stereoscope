b5ab16c4525b091219f075da9021eef61a33d2e3
Christian Laesser <mail@christianlaesser.com>
2018-05-25 11:47:15 +0200

    Layouts: network fixed

--- src/jsx/layouts/network.jsx
+++ src/jsx/layouts/network.jsx
@@ -12,7 +12,7 @@ export default {
   inputs: [],
   create: function grid(glyphs, width, height, options) {
     // vars
-    const margins = { top: 30, bottom: 150, right: 30, left: 30 };
+    const margins = { top: 30, bottom: 30, right: 30, left: 30 };
     const stageWidth = width - margins.left - margins.right;
     const stageHeight = height - margins.top - margins.bottom;
 
@@ -28,15 +28,16 @@ export default {
       }
     });
 
-    // // box boundaries
-    // const boxForce = () => {
-    //   for (let i = 0, n = glyphs.length; i < n; i++) {
-    //     const d = glyphs[i];
-    //     d.x = Math.max(d.radius, Math.min(stageWidth - d.radius, d.x));
-    //     d.y = Math.max(d.radius, Math.min(stageHeight - d.radius, d.y));
-    //   }
-    // };
-    // start simulation
+    // box boundaries
+    const boxForce = () => {
+      for (let i = 0, n = glyphs.length; i < n; i++) {
+        const d = glyphs[i];
+        d.x = Math.max(d.radius, Math.min(stageWidth - d.radius, d.x));
+        d.y = Math.max(d.radius, Math.min(stageHeight - d.radius, d.y));
+      }
+    };
+
+    //start simulation
 
     // const ticked = () => {
     //   // glyphs.attr('transform', function(d) {
@@ -57,7 +58,7 @@ export default {
       .force('center', forceCenter(stageWidth / 2, stageHeight / 2))
       .force('collide', forceCollide(d => d.radius * 1.2))
       // .on('tick', ticked)
-      // .force('box_force', boxForce)
+      .force('box_force', boxForce)
       .stop();
 
     for (var i = 0; i < 400; ++i) simulation.tick();
